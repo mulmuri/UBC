89cbe5f34e77bc7ef810710513729cfefe04badf
mulmuri <68294539+mulmuri@users.noreply.github.com>
2022-05-13 17:46:43 +0900

    add script & video file download ui

--- src/downloadRouter.js
+++ src/downloadRouter.js
@@ -3,10 +3,23 @@ const router = express.Router();
 
 const file_info = require("../data/file_info.json");
 
+getExtensionName = {
+    material: "pdf",
+    video: "mp4",
+    script: "docx"
+}
 
-router.get('/download/:lectureNo', function(request, response) {
+router.get('/download/:lectureNo/:filetype', function(request, response) {
     var lectureNo = request.params.lectureNo;
-    response.download(`data/attachment/UBC ${lectureNo} - ${file_info.video[lectureNo].title}.pdf`);
+    var filetype = request.params.filetype;
+    var extensionName = getExtensionName[filetype]
+    
+    response.download(`data/${filetype}/UBC ${lectureNo} - ${file_info.video[lectureNo].title}.${extensionName}`);
+})
+
+router.get('download/video/:lectureNo', function(request, response) {
+    var lectureNo = request.params.lectureNo;
+    response.download(`data/video/`)
 })
 
 module.exports = router;
